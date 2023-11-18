60f745adb4cc12d3d8ca1d5decd3908e15825592
Eric Huang <eric.huang3143@gmail.com>
2023-11-18 15:02:05 -0500

    created connector

---
+++ backend/server/controller/connector.py
@@ -0,0 +1,69 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../model"))
+
+import model.MqttHandler as mq
+import threading
+import queue
+import pandas as pd
+from typing import Tuple
+
+
+# Define a function to process messages from the queue and update DataFrames
+def process_data(message_queue: queue.Queue, 
+                 truck_df: pd.DataFrame, 
+                 load_df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+    while not message_queue.empty():
+        json_data = message_queue.get()
+        temp_df = pd.DataFrame.from_dict(json_data, orient='index').T
+        tp = temp_df['type'].item().lower()
+
+        if tp == 'start':
+            # Reset DataFrames for new day
+            truck_df = pd.DataFrame(columns=['seq', 'type', 'timestamp', 'truckId', 'positionLatitude', 'positionLongitude', 'equipType', 'nextTripLengthPreference'])
+            load_df = pd.DataFrame(columns=['seq', 'type', 'timestamp', 'loadId', 'originLatitude', 'originLongitude', 'destinationLatitude', 'destinationLongitude', 'equipmentType', 'price', 'mileage'])
+            print("Start of new day")
+        elif tp == 'truck':
+            print("Got truck")
+            truck_df = pd.concat([truck_df, temp_df], ignore_index=True)
+        elif tp == 'load':
+            print("Got load")
+            load_df = pd.concat([load_df, temp_df], ignore_index=True)
+        elif tp == 'end':
+            print("End of day")
+
+    # Return DataFrames in case they need to be used immediately after calling
+    return truck_df, load_df
+
+# Define global DataFrames
+truck_df = pd.DataFrame()
+load_df = pd.DataFrame()
+
+# Thread-Safe Queue
+message_queue = queue.Queue()
+
+# Set Up Handler
+handler = mq.MqttHandler(message_queue)
+
+# Threads Setup
+listener_thread = threading.Thread(target=handler.listen)
+
+# Start Listener Thread
+listener_thread.start()
+
+if __name__ == '__main__':
+    # Example usage of process_data function
+    def periodically_process_data():
+        import time
+        while True:
+            if not message_queue.empty():
+                truck_data, load_data = process_data(message_queue)
+                # You can now use truck_data and load_data DataFrames
+                # For example, print them, analyze, or save to CSV
+                # print(truck_data, load_data)
+            time.sleep(5)  # Process every 5 seconds
+
+    # Start periodic processing in a separate thread
+    processor_thread = threading.Thread(target=periodically_process_data)
+    processor_thread.start()
--- backend/server/controller/mqtt_client.py
+++ backend/server/controller/mqtt_client.py
@@ -11,7 +11,7 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "CodeJam"
 MQTT_USERNAME = "CodeJamUser"
 MQTT_PASSWORD = "123CodeJam"
-CLIENT_ID = "GDSC01"  # Replace with your team name
+CLIENT_ID = "GDSC01"
 
 # Output Format and Folder Settings
 OUTPUT_FORMAT = "csv"  # Change to "json" for JSON output
@@ -20,67 +20,53 @@ MAX_FILES_PER_DATE = 10
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
 
-# Global variables
-current_file = None
-file_counters = {}
-
-# Function to open a new file for the given date
-def open_new_file(date_str):
-    # Increment the counter for this date
-    file_counters[date_str] = file_counters.get(date_str, 0) + 1
-    if file_counters[date_str] > MAX_FILES_PER_DATE:
-        print(f"Maximum number of files reached for {date_str}. No new file will be created.")
-        return None
-
-    # Create file name with counter
-    counter_str = f"_{file_counters[date_str]:04d}" if file_counters[date_str] > 1 else ""
-    file_name = f"mqtt_data_{date_str}{counter_str}.{OUTPUT_FORMAT}"
-    file_path = os.path.join(OUTPUT_FOLDER, file_name)
+class MqttHandler:
+    def __init__(self):
+        self.truck = pd.DataFrame(columns=['seq', 'type', 'timestamp', 'truckId', 'positionLatitude', 'positionLongitude', 'equipType', 'nextTripLengthPreference'])
+        self.load = pd.DataFrame(columns=['seq', 'type', 'timestamp', 'loadId', 'originLatitude', 'originLongitude', 'destinationLatitude', 'destinationLongitude', 'equipmentType', 'price', 'mileage'])
+        self.cur_day = 0
     
-    file = open(file_path, 'w', newline='', encoding='utf-8')
-    if OUTPUT_FORMAT == "json":
-        json.dump([], file)  # Initialize with an empty list
-    return file
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe(MQTT_TOPIC)
 
-# Callback for when the client receives a CONNACK response from the server
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe(MQTT_TOPIC)
+    def on_message(self, client, userdata, msg):
+        json_data = json.loads(msg.payload)
 
-# Callback for when a PUBLISH message is received from the server
-def on_message(client, userdata, msg):
-    global current_file
+        # Convert Python object to Pandas DataFrame
+        temp_df = pd.DataFrame.from_dict(json_data, orient='index').T
+        tp = temp_df['type'].item()
+        
+        if tp == 'Start':
+            self.truck = pd.DataFrame(columns=['seq', 'type', 'timestamp', 'truckId', 'positionLatitude', 'positionLongitude', 'equipType', 'nextTripLengthPreference'])
+            self.load = pd.DataFrame(columns=['seq', 'type', 'timestamp', 'loadId', 'originLatitude', 'originLongitude', 'destinationLatitude', 'destinationLongitude', 'equipmentType', 'price', 'mileage'])
+            print("Start of new day")
+            self.cur_day += 1
+        elif self.truck is not None and self.load is not None:
+            if tp == 'Truck':
+                print("Got truck")
+                self.truck = pd.concat([self.truck, temp_df])
+                #self.truck.append(temp_df, ignore_index=True)
+            elif tp == 'Load':
+                print("Got load")
+                self.load = pd.concat([self.load, temp_df])
+                #self.load.append(temp_df, ignore_index=True)
+            elif tp == 'End':
+                print("End of day")
+                self.truck.to_csv(f'mqtt_data_2/truck_{self.cur_day}.csv', index=False)
+                self.load.to_csv(f'mqtt_data_2/load_{self.cur_day}.csv', index=False)
 
-    print(f"Message received on topic {msg.topic}")
-
-    # Convert message payload to a Python dictionary
-    message_data = json.loads(msg.payload)
-
-    # Handle Start event
-    if message_data["type"] == "Start":
-        date_str = datetime.fromisoformat(message_data["timestamp"]).strftime("%Y-%m-%d")
-        if current_file is not None:
-            current_file.close()
-        current_file = open_new_file(date_str)
-
-    # Write data to file for other message types
-    elif current_file is not None and message_data["type"] != "End":
-        if OUTPUT_FORMAT == "csv":
-            writer = csv.writer(current_file)
-            writer.writerow([message_data.get(key, '') for key in sorted(message_data)])
-        elif OUTPUT_FORMAT == "json":
-            current_file.seek(0)
-            data = json.load(current_file)
-            data.append(message_data)
-            current_file.seek(0)
-            current_file.truncate()
-            json.dump(data, current_file)
+                self.truck = None
+                self.load = None
+        else:
+            print("Got message, not start of day")
 
 # Create an MQTT client and attach the callback functions
 client = mqtt.Client(client_id=CLIENT_ID, clean_session=True)
 client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
-client.on_connect = on_connect
-client.on_message = on_message
+handler = MqttHandler()
+client.on_connect = handler.on_connect
+client.on_message = handler.on_message
 
 # Connect to the MQTT broker
 client.connect(MQTT_BROKER, MQTT_PORT, 60)

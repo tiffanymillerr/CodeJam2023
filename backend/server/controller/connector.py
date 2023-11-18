37fd82eae06af8da1f123067a464e906251ec96a
Eric Huang <eric.huang3143@gmail.com>
2023-11-18 16:19:03 -0500

    fixed import issue

--- backend/server/controller/connector.py
+++ backend/server/controller/connector.py
@@ -1,13 +1,53 @@
 import sys
 import os
-sys.path.append(os.path.abspath("../model"))
-
-import model.MqttHandler as mq
+# sys.path.append(os.path.abspath("../model"))
+# import model.MqttHandler as mq
 import threading
 import queue
 import pandas as pd
 from typing import Tuple
 
+
+import paho.mqtt.client as mqtt
+import json
+
+class MqttHandler:
+    # MQTT Settings
+    MQTT_BROKER = "fortuitous-welder.cloudmqtt.com"
+    MQTT_PORT = 1883
+    MQTT_TOPIC = "CodeJam"
+    MQTT_USERNAME = "CodeJamUser"
+    MQTT_PASSWORD = "123CodeJam"
+    CLIENT_ID = "GDSC01"  # Replace with your team name
+
+    def __init__(self, queue):
+        self.queue = queue
+        self.cur_day = 0
+    
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe(self.MQTT_TOPIC)
+
+    def on_message(self, client, userdata, msg):
+        json_data = json.loads(msg.payload)
+
+        if json_data["type"].lower() == "start":
+            json_data["cur_day"] = self.cur_day
+            self.cur_day += 1
+
+        # Put the received message into the queue
+        self.queue.put(json_data)
+
+        # Example to print queue contents
+        print(list(self.queue.queue))
+
+    def listen(self):
+        client = mqtt.Client(client_id=self.CLIENT_ID, clean_session=True)
+        client.username_pw_set(self.MQTT_USERNAME, self.MQTT_PASSWORD)
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.connect(self.MQTT_BROKER, self.MQTT_PORT, 60)
+        client.loop_forever()
 
 # Define a function to process messages from the queue and update DataFrames
 def process_data(message_queue: queue.Queue, 
@@ -44,7 +84,7 @@ load_df = pd.DataFrame()
 message_queue = queue.Queue()
 
 # Set Up Handler
-handler = mq.MqttHandler(message_queue)
+handler = MqttHandler(message_queue)
 
 # Threads Setup
 listener_thread = threading.Thread(target=handler.listen)

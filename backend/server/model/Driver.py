6d141b3e24a10eeb9988793e8646e87f775b712b
Tiffany <tiffanymiller0708@gmail.com>
2023-11-18 16:47:34 -0500

    added truck and load id

--- backend/server/model/Driver.py
+++ backend/server/model/Driver.py
@@ -1,5 +1,6 @@
 class Driver:
-    def __init__(self, location, equip_type, trip_length_preference):
+    def __init__(self, truck_id, location, equip_type, trip_length_preference):
+        self.truck_id = truck_id
         self.location = location
         self.equip_type = equip_type
         self.trip_length_preference = trip_length_preference
--- backend/server/model/Load.py
+++ backend/server/model/Load.py
@@ -1,5 +1,6 @@
 class Load:
-    def __init__(self, price, mileage, equipment_type, origin, destination):
+    def __init__(self, load_id, price, mileage, equipment_type, origin, destination):
+        self.load_id = load_id
         self.price = price
         self.mileage = mileage
         self.equipment_type = equipment_type

a862b5af59ee59515f4202fd889bead2316579b6
Simard302 <Simard302@users.noreply.github.com>
2023-11-19 11:26:27 -0500

    Added grayscale and contrast + made videos

--- matrox/num_predict.py
+++ matrox/num_predict.py
@@ -3,7 +3,17 @@ import cv2
 from google.cloud import vision
 client = vision.ImageAnnotatorClient()
 
-cap = cv2.VideoCapture('output_video.mp4')
+
+filename = "codejam_matrox_2023_noisy"
+
+cap = cv2.VideoCapture(f'{filename}_denoised.mp4')
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+output_grayscale = cv2.VideoWriter(f'{filename}_grayscale.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, frame_size, isColor=True)
+output_contrast = cv2.VideoWriter(f'{filename}_contrast.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, frame_size, isColor=True)
+
 allTexts = []
 fi = 0
 while cap.isOpened():
@@ -11,7 +21,14 @@ while cap.isOpened():
     if not ret: break
     fi += 1
 
-    _, img_buffer = cv2.imencode('.jpg', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    output_grayscale.write(gray)
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    clahe_output = clahe.apply(gray)
+    output_contrast.write(clahe_output)
+
+    _, img_buffer = cv2.imencode('.jpg', clahe_output)
 
 
     content = img_buffer.tobytes()
